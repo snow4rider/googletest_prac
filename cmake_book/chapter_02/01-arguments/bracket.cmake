36f33e20b00cd95c0e5bfa37036c5bdf09e6df14
Matthew <matthew.t.mckenney@gmail.com>
2026-04-11 04:48:54 +0000

    working on chapter 2

---
+++ cmake_book/chapter_02/01-arguments/bracket.cmake
@@ -0,0 +1,11 @@
+message([[multiline
+bracket
+argument
+]])
+message([==[
+    because we used two equal-signs "=="
+    this command recieves only a single argument
+    even if it includes two square brackets in a row
+    { "petsArray" = [["mouse", "cat"],["dog"]] }
+
+]==])
